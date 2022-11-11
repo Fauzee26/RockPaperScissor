76672e8b58a838768bb970a5f7c03782f489a8d4
hilmyFauzi <hilmy@mhcasiagroup.com>
2022-11-11 08:08:26 +0700

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Muhammad Hilmy Fauzi on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
